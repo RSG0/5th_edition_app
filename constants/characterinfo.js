97f12ba133869155fb9c8253dd8870f70b153af8
Abubakar Kassim <142273851+RSG0@users.noreply.github.com>
2024-07-04 01:33:34 -0500

    Update characterinfo.js

--- constants/characterinfo.js
+++ constants/characterinfo.js
@@ -12,15 +12,45 @@ const CLASSES =
     {label: "Sorceror", value: '10'},
     {label: "Wizard", value: '11'}
 ]
-const RACES =
-[
+const RACES = [
     {label: "Aarakocra", value: '1'},
     {label: "Aasimar", value: '2'},
-    {label: "Changeling", value: '3'},
-    {label: "Dragonborn", value: '4'},
-    {label: "Dwarf", value: '5'},
-    {label: "Elf", value: '6'}
-]
+    {label: "Bugbear", value: '3'},
+    {label: "Centaur", value: '4'},
+    {label: "Changeling", value: '5'},
+    {label: "Dragonborn", value: '6'},
+    {label: "Dwarf", value: '7'},
+    {label: "Elf", value: '8'},
+    {label: "Firbolg", value: '9'},
+    {label: "Genasi", value: '10'},
+    {label: "Gith", value: '11'},
+    {label: "Gnome", value: '12'},
+    {label: "Goblin", value: '13'},
+    {label: "Goliath", value: '14'},
+    {label: "Half-Elf", value: '15'},
+    {label: "Half-Orc", value: '16'},
+    {label: "Halfling", value: '17'},
+    {label: "Hobgoblin", value: '18'},
+    {label: "Human", value: '19'},
+    {label: "Kalashtar", value: '20'},
+    {label: "Kenku", value: '21'},
+    {label: "Kobold", value: '22'},
+    {label: "Lizardfolk", value: '23'},
+    {label: "Loxodon", value: '24'},
+    {label: "Minotaur", value: '25'},
+    {label: "Orc", value: '26'},
+    {label: "Satyr", value: '27'},
+    {label: "Shifter", value: '28'},
+    {label: "Tabaxi", value: '29'},
+    {label: "Tiefling", value: '30'},
+    {label: "Tortle", value: '31'},
+    {label: "Triton", value: '32'},
+    {label: "Warforged", value: '33'},
+    {label: "Yuan-ti Pureblood", value: '34'}
+];
+
+console.log(RACES);
+
 const LEVELS = 
 [ 
 
