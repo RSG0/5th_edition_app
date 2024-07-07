adeb3ba10ec3f48f24714ab950e4322789af5de9
Abubakar Kassim <redtreakg@gmail.com>
2024-07-07 00:31:36 -0500

    used bottom tab and worked on top tabs

--- app/index.js
+++ app/index.js
@@ -7,59 +7,56 @@ import CharacterPage from "./CharacterPage";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { COLORS } from "../constants/theme";
 import { Header } from "react-native/Libraries/NewAppScreen";
-import { createNativeStackNavigator, createBottomTabNavigator, } from "@react-navigation/native-stack";
+import { createNativeStackNavigator, } from "@react-navigation/native-stack";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+// import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { StyleSheet } from "react-native-web";
 import DiceRoller from "./DiceRoller";
 import SettingPage from "./SettingPage";
 import { createContext } from "react";
+import CompendiumPage from "./CompendiumPage";
 
 
 const Stack = createNativeStackNavigator();
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 function TabNavigator() 
 {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Character Page" component={CharacterPage}/>
+        <Tab.Navigator screenOptions={{tabBarStyle: {backgroundColor: COLORS.mainColor}, headerShown:false}}>
+            <Tab.Screen name="Character Page" component={StackNavigator} options={{headerShown:true}} />
             <Tab.Screen name="Compendium Page" component={CompendiumPage}/>
             <Tab.Screen name="Dice Roller Page" component={DiceRoller}/>
-            <Tab.Screen name="Dice Roller Page" component={SettingPage}/>            
+            <Tab.Screen name="Setting Page" component={SettingPage}/>            
         </Tab.Navigator>
     )
 }
 function StackNavigator()
 {
-    <Stack.Navigator>
+    return(
+    <Stack.Navigator 
+        initialRouteName="Create Character" screenOptions={{
+        headerStyle: {
+            backgroundColor: COLORS.mainColor,
+            paddingBottom: 20, 
+            // height: 200
+        },
+        statusBarColor: "red",
+        headerTintColor: 'white',
+        headerBackVisible: false
+    }}>
                 <Stack.Screen name="Create Character" component={CreateCharacter} options={{ }}/>
                 <Stack.Screen name="Ability Score" component={AbilityScoreScreen}/>
                 <Stack.Screen name="Select Skills" component={SelectingSkillsScreen}/>    
-    </Stack.Navigator>
+    </Stack.Navigator>)
 }
 const App = () =>
 {
     return(
-    
-    <NavigationContainer independent={true}>
-            <Stack.Navigator initialRouteName="Create Character" screenOptions={{
-                headerStyle: {
-                    backgroundColor: COLORS.mainColor,
-                    paddingBottom: 20, 
-                    height: 200
-                },
-                statusBarColor: "red",
-                headerTintColor: 'white',
-                headerBackVisible: false
-            }}>
-                <Stack.Screen name="Create Character" component={CreateCharacter} options={{ }}/>
-                <Stack.Screen name="Ability Score" component={AbilityScoreScreen}/>
-                <Stack.Screen name="Select Skills" component={SelectingSkillsScreen}/>
-            </Stack.Navigator>
-
+        <NavigationContainer independent={true}>
+            <TabNavigator/>
         </NavigationContainer>
-
     )
 }
 export default App;
