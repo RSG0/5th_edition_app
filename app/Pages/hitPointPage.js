8f0422e9659f4b7ee178f8f51709ea2f888043ec
Abubakar Kassim <redtreakg@gmail.com>
2024-08-04 15:26:28 -0500

    added hitpoint screen

---
+++ app/Pages/hitPointPage.js
@@ -0,0 +1,34 @@
+import { View, StyleSheet, Text, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { COLORS, FONTSIZE } from "../../constants/theme";
+
+export default HitPointPage = () =>
+{
+    return (
+        <SafeAreaView style={{backgroundColor: COLORS.background, flex: 1}}>
+        {/* <ScrollView> */}
+        <View style={styles.viewStyle}>
+            <Text style={styles.textStyle}>This is the Hit Point Screen</Text>
+        </View>
+        {/* </ScrollView> */}
+        </SafeAreaView>
+    );
+}
+const styles = StyleSheet.create(
+{
+    viewStyle: {
+        
+        backgroundColor: COLORS.background,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1
+    },
+    textStyle: {
+        fontWeight: 'bold',
+        fontSize: FONTSIZE.xxlarge,
+        textAlign: 'center'
+    },
+
+}
+)
