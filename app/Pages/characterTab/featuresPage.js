2ade5fb6ab030b361eec0027d3fd60ef79c63048
Abubakar Kassim <redtreakg@gmail.com>
2024-08-24 18:27:03 -0500

    added more features in the form of icons squares

--- app/Pages/characterTab/featuresPage.js
+++ app/Pages/characterTab/featuresPage.js
@@ -1,9 +1,8 @@
-import { View, StyleSheet, Text, ScrollView, TextInput, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Text, ScrollView, TextInput, TouchableOpacity, Platform, Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { COLORS, FONT, FONTSIZE } from "../../../constants/theme";
 import { useEffect, useState } from "react";
 import * as ImagePicker from 'expo-image-picker'
-import { ImagePickerIOS } from "react-native";
 
 export default FeaturesPage = () =>
 {
@@ -19,13 +18,24 @@ export default FeaturesPage = () =>
 
     useEffect(() =>
     {
-
+        (async() => {
+            if (Platform.OS === 'ios')
+                {
+                    console.log("Working...")
+                    const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync()
+                    if (status !== "granted")
+                    {
+                        Alert.alert("Permission Required", "Sorry, we need camera permissions to make this work!");
+                    }
+                }
         
+        })
+
     }, [])
 
     const handleImagePickerPress = async() =>
     {
-        let result = await ImagePicker.launchCameraAsync({
+        let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
             allowsEditing: true,
             aspect: [1,1],
@@ -60,9 +70,19 @@ export default FeaturesPage = () =>
         <SafeAreaView style={{backgroundColor: COLORS.background, flex: 1}}>
         <ScrollView>
         <View style={styles.background}>
-            <TouchableOpacity style={styles.square} onPress={handleImagePickerPress}>
-                <Text style={styles.imageText} >press image here ...</Text>
-            </TouchableOpacity>
+            <View style={styles.imageContainer}>
+                <TouchableOpacity style={styles.imageSquare} onPress={handleImagePickerPress}>
+                    <Text style={styles.imageText} >press image here ...</Text>
+                </TouchableOpacity>
+                <View style={{margin: 20}} />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.iconContainer}>
+                            <TouchableOpacity style={styles.iconSquare}></TouchableOpacity>
+                            <TouchableOpacity style={styles.iconSquare}></TouchableOpacity>
+                        </View>
+                        <TouchableOpacity style={styles.resetButton}></TouchableOpacity>
+                    </View>
+            </View>
             {borderBubble("Personality Traits", personalityTraits, setPersonalityTraits)}
             {borderBubble("Bonds", bonds, setBonds)}
             {borderBubble("Ideals", ideals, setIdeals)}
@@ -80,9 +100,12 @@ export default FeaturesPage = () =>
         </SafeAreaView>
     );
 }
+
+const imageSquareSize = 150
+const iconSquareSize = 40
+
 const styles = StyleSheet.create(
 {
-    const: squareSize = 150,
 
     background: {        
         backgroundColor: '#acacac',
@@ -94,6 +117,12 @@ const styles = StyleSheet.create(
         justifyContent: 'center',
         alignItems: 'center',
     },
+    imageContainer: {
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignSelf: 'center',
+        flexDirection: 'row'
+    },
     textBorder: {
         width: "100%",
         minHeight: 120,
@@ -104,14 +133,20 @@ const styles = StyleSheet.create(
         marginBottom: 10
 
     },
+    iconContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignContent: 'center',
+        flexDirection: 'row'
+    },
     textInput: {
-        width: "100%",
+        // width: "100%",
         fontStyle: 'italic',
         flex: 1,
         padding: 10,
         // backgroundColor: 'red',
         fontSize: FONTSIZE.medium,
-        flexWrap: 'wrapf'
+        // flexWrap: 'wrapf'
     },
     textStyle: {
         fontWeight: 'bold',
@@ -119,17 +154,24 @@ const styles = StyleSheet.create(
         textDecorationLine: 'underline',
         textAlign: 'center'
     },
-    square: {
-        width: squareSize,
-        height: squareSize,
-        backgroundColor: 'white',
+    iconSquare: {
+        width: iconSquareSize,
+        height: iconSquareSize,
+        backgroundColor: '#9D9D9D', //gray
+        margin: 10,
+        borderRadius: 10,
+    },
+    imageSquare: {
+        width: imageSquareSize,
+        height: imageSquareSize,
+        backgroundColor: '#9D9D9D', //gray
         borderRadius: 20,
         marginBottom: 20,
 
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        flex: 1
+        // flex: 1
     },
     imageText: {
         width: 80,
@@ -137,6 +179,18 @@ const styles = StyleSheet.create(
         color: 'grey',
         alignItems: 'center',
         textAlign: 'center',
+    },
+    buttonContainer: {
+        flexDirection: 'column',
+        alignContent: 'center',
+        alignSelf: 'center'
+    },
+    resetButton:{
+        width: 90,
+        height: 40,
+        borderRadius: 30,
+        backgroundColor: '#9D9D9D', //gray
+        marginLeft: 15
     }
 }
 )
