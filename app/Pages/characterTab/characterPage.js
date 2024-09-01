67d342fdf2c8bd842bbf2deecdb917c0efaf42f2
Abubakar Kassim <redtreakg@gmail.com>
2024-09-01 11:16:45 -0500

    characterPage can now display multiple characters

--- app/Pages/characterTab/characterPage.js
+++ app/Pages/characterTab/characterPage.js
@@ -1,53 +1,52 @@
-import { View, StyleSheet, Text, ScrollView, TouchableOpacity } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, ScrollView, SafeAreaView, As } from "react-native";
 import { COLORS, FONTSIZE } from "../../../constants/theme";
-
-import NewCharacterIcon_Rough from "../../../components/newCharacterIcon(Rough)";
 import NewCharacterIcon from "../../../components/newCharacterIcon";
 import NewCharacterButton from "../../../components/buttons/newCharacterButton";
 
-export default CharacterPage = ({navigation, route}) =>
-{
+export default CharacterPage = ({ navigation, route }) => {
+    const [characters, setCharacters] = useState([
+        // Initial characters can be added here or left empty
+    ]);
+
+
     const { name, classes, level, selectedRace, image } = route.params || {};
 
+    useEffect(() => {
+        if (name) {
+            const newCharacter = { name, classes, level, race: selectedRace, image };
+            setCharacters(prevCharacters => [...prevCharacters, newCharacter]);
+        }
+    }, [name, classes, level, selectedRace, image]);
+
     return (
-        <SafeAreaView style={{backgroundColor: COLORS.background, flex: 1}}>
-        <ScrollView>
-        <View style={styles.viewStyle}>
-            {/* <Text style={styles.textStyle}>This is the Character Screen</Text> */}
-            <NewCharacterIcon_Rough/>
-            <NewCharacterIcon_Rough/>
-            <NewCharacterIcon_Rough/>
-            {name && (<NewCharacterIcon
-                image={image}
-                name={name}
-                classes={classes}
-                level={level}
-                race={selectedRace}/>)}
-
-
-
-        </View>
-        </ScrollView>
-        <NewCharacterButton navigation={navigation} nextScreen={"Create Character"}/>
+        <SafeAreaView style={{ backgroundColor: COLORS.background, flex: 1 }}>
+            <ScrollView>
+                <View style={styles.viewStyle}>
+                    {characters.map((character, index) => (
+                        <NewCharacterIcon
+                            key={index}
+                            name={character.name}
+                            classes={character.classes}
+                            level={character.level}
+                            race={character.race}
+                            image={character.image}
+                        />
+                    ))}
+                </View>
+            </ScrollView>
+            <NewCharacterButton navigation={navigation} nextScreen={"Create Character"} />
         </SafeAreaView>
     );
 }
+
 const addSize = 60;
-const styles = StyleSheet.create(
-{
+const styles = StyleSheet.create({
     viewStyle: {
-        
         backgroundColor: COLORS.background,
         display: 'flex',
-        // justifyContent: 'center',
         alignItems: 'center',
-        flex: 1
-    },
-    textStyle: {
-        fontWeight: 'bold',
-        fontSize: FONTSIZE.xxlarge,
-        // textAlign: 'center'
+        flex: 1,
     },
     addButton: {
         justifyContent: 'center',
@@ -58,9 +57,6 @@ const styles = StyleSheet.create(
         borderRadius: 40,
         borderWidth: 5,
         position: 'absolute',
-        bottom: 10
-
-    }
-
-}
-)
+        bottom: 10,
+    },
+});
