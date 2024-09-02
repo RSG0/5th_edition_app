86a8c0d1b21437a0a20fa7649385ce21bb9a090f
Abubakar Kassim <redtreakg@gmail.com>
2024-09-02 14:15:46 -0500

    added dummy values and resized and refined design

--- components/characterComponents/sensesIcon.js
+++ components/characterComponents/sensesIcon.js
@@ -1,18 +1,20 @@
 import { View, StyleSheet, Text } from "react-native";
 import { characterBorderWidth, COLORS, FONTSIZE } from "../../constants/theme";
 
-export default SensesIcon = ({name, isProfcient, modBonus}) =>
+export default SensesIcon = ({abilityName, isProfcient, mod}) =>
 {
+    const displayAbilityName = abilityName || "Passive (NNN): Nvestigation"
+    const displayIsProfcient = isProfcient || true
+    const displayMod = mod || "2N"
+
     return (
-        <View style={[styles.viewStyle, {marginTop: 20}]}>
+        <View style={[styles.viewStyle, {margin: 15}]}>
             <Text style={styles.textStyle} >
-                STRENGTH{"\n"} 
+                {displayAbilityName}{"\n"} 
             </Text>
-            <View style={styles.profcientContainer}>
-                <View style={styles.profcientMark}></View>
-            </View>
+
             <View style={styles.modifierContainer}>
-                <Text style={styles.modifierText}>+3</Text>
+                <Text style={styles.modifierText}>{displayMod}</Text>
             </View>
         </View>
     );
@@ -21,8 +23,8 @@ export default SensesIcon = ({name, isProfcient, modBonus}) =>
 const styles = StyleSheet.create(
 {
     viewStyle: {
-        width: 400,
-        height: 50,
+        width: 300,
+        height: 30,
         backgroundColor: COLORS.mainColor,
         justifyContent: 'center',
         alignSelf: 'center',
@@ -32,40 +34,22 @@ const styles = StyleSheet.create(
     textStyle: {
         textAlign: 'center',
         fontWeight: 'bold',
-        fontSize: FONTSIZE.xxlarge,
-    },
-    profcientContainer: {
-        position: 'absolute',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 40,
-        height: 30,
-        left: -20,
-        backgroundColor: COLORS.mainColor,
-        borderRadius: 30,
-        borderWidth: characterBorderWidth
-    },
-    profcientMark: {
-        alignSelf: 'center',
-        width: 20,
-        height: 15,
-        backgroundColor: 'black',
-        borderRadius: 30,
+        fontSize: FONTSIZE.medium,
     },
     modifierContainer: {
         position: 'absolute',
         justifyContent: 'center',
         alignItems: 'center', // Center horizontally
-        width: 100,
-        height: 70,
+        width: 50,
+        height: 50,
         backgroundColor: COLORS.mainColor,
         left: -20,
-        borderWidth: characterBorderWidth + 5,
+        borderWidth: characterBorderWidth,
         borderRadius: 50 
     },
     modifierText: {
         textAlign: 'center',
         fontWeight: 'bold',
-        fontSize: FONTSIZE.xxlarge + 10
+        fontSize: FONTSIZE.xlarge
     }
 });
